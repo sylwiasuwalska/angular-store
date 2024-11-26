194140085c204b649c618950200ff82264aa2c5b
sylwia.suwalska <sylwia.suwalska@olx.pl>
2024-11-26 14:23:12 +0100

    feature: add ngModelChange

--- src/app/order-form/order-form.component.ts
+++ src/app/order-form/order-form.component.ts
@@ -8,18 +8,26 @@ export class OrderFormComponent implements OnInit {
   fullName: string = '';
   address: string = '';
   cardNumber: string = '';
+  agreement: boolean = false;
   @Output() submitOrder: EventEmitter<any> = new EventEmitter();
   constructor() {}
   ngOnInit(): void {}
+
+  onAgreementChange(event: boolean) {
+    this.agreement = event;
+    console.log('Agreement changed:', this.agreement);
+  }
   submitForm(): void {
-    const order = {
-      fullName: this.fullName,
-      address: this.address,
-      cardNumber: this.cardNumber,
-    };
-    this.submitOrder.emit(order);
-    this.fullName = '';
-    this.address = '';
-    this.cardNumber = '';
+    if (this.agreement) {
+      const order = {
+        fullName: this.fullName,
+        address: this.address,
+        cardNumber: this.cardNumber,
+      };
+      this.submitOrder.emit(order);
+      this.fullName = '';
+      this.address = '';
+      this.cardNumber = '';
+    }
   }
 }
